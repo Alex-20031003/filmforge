826535bbabc4f057d6af518882ddb34026aa4931
Алексей Руденок <oleksiirudenok@gmail.com>
2026-09-12 13:21:34 +0200

    chore(backend): initialize Express and TypeScript setup

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+export const app = express()
+
+app.use(express.json())
+
+// app.get('/health', (req, res) => {
+//   res.status(200).json({
+//     message: 'Server is alive'
+//   })
+// })
+
