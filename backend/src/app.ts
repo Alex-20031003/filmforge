1eaa4ce18ae09c91eff5e3045ae7cba101b66d5f
Алексей Руденок <oleksiirudenok@gmail.com>
2026-09-15 18:06:11 +0200

    feat: add backend health endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,8 @@
 import express from 'express';
+import { router as healthRouter } from './modules/health/health.routes.js'
 
 export const app = express()
 
 app.use(express.json())
 
-// app.get('/health', (req, res) => {
-//   res.status(200).json({
-//     message: 'Server is alive'
-//   })
-// })
-
+app.use('/health', healthRouter)
